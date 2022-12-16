5ffd55afe953516a8f93ca7b81bc303a03d53718
Emilis <emiliskleinas2003@gmail.com>
2022-12-16 13:03:34 +0200

    Implemented doubly linked list

--- namu darbai/nd4/double_linked_list.c
+++ namu darbai/nd4/double_linked_list.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct DoubleLinkedList
+typedef struct Node
 {
     void *value;
     size_t valueSize;
-    struct DoubleLinkedList *prev;
-    struct DoubleLinkedList *next;
-} DoubleLinkedList;
+    struct Node *prev;
+    struct Node *next;
+} Node;
 
 //          TO DO
 //  1. Refactor so last node search for setValueByIndex(), insertValueAtIndex() is in function
@@ -23,17 +23,17 @@ void coppyBytes(void *destination, void *source, size_t size)
 }
 
 // Creates double linked list. If fails returns NULL pointer
-DoubleLinkedList *initDLList(void *value, size_t valueSize)
+int initDLList(Node **head, Node **tail, void *value, size_t valueSize)
 {
     void *valueLocation = malloc(valueSize);
 
     coppyBytes(valueLocation, value, valueSize);
 
-    DoubleLinkedList *newList = (DoubleLinkedList *)malloc(sizeof(DoubleLinkedList));
+    Node *newList = (Node *)malloc(sizeof(Node));
 
     if (newList == NULL)
     {
-        return NULL;
+        return -1;
     }
 
     newList->value = valueLocation;
@@ -41,13 +41,15 @@ DoubleLinkedList *initDLList(void *value, size_t valueSize)
     newList->prev = NULL;
     newList->next = NULL;
 
-    return newList;
+    *head = newList;
+    *tail = newList;
+    return 0;
 }
 
-// Takes root of list. Deletes all elements that follow
-void deleteDLList(DoubleLinkedList *DLList)
+// Takes tail of list. Deletes all elements that follow
+void deleteDLList(Node *DLList)
 {
-    DoubleLinkedList *next;
+    Node *next;
     next = DLList->next;
 
     if (next != NULL)
@@ -60,7 +62,7 @@ void deleteDLList(DoubleLinkedList *DLList)
 }
 
 // Takes start node. Returns the length of list
-size_t getDLListLen(DoubleLinkedList *Dll)
+size_t getDLListLen(Node *Dll)
 {
     size_t length = 0;
 
@@ -74,7 +76,7 @@ size_t getDLListLen(DoubleLinkedList *Dll)
 }
 
 // Reuturns uncast pointer to value at index or NULL on fail
-void *getValueByIndex(DoubleLinkedList *Dll, size_t index)
+void *getValueByIndex(Node *Dll, size_t index)
 {
     int i;
     for (i = 0; i < index; ++i)
@@ -90,7 +92,7 @@ void *getValueByIndex(DoubleLinkedList *Dll, size_t index)
 }
 
 // Sets value of double linked list at index. Reutrns -1 on fail
-int setValueAtIndex(DoubleLinkedList *Dll, size_t index, void *newValue, size_t valueSize)
+int setValueAtIndex(Node *Dll, size_t index, void *newValue, size_t valueSize)
 {
     int i;
     for (i = 0; i < index; ++i)
@@ -113,47 +115,226 @@ int setValueAtIndex(DoubleLinkedList *Dll, size_t index, void *newValue, size_t 
     return 0;
 }
 
-// Returns 0 on successful insertion. -1 if fails
-int insertDLLValueAtIndex(DoubleLinkedList *Dll, size_t index, void *newValue, size_t valueSize)
+int addToTail(Node **tail, void *value, size_t valueSize)
 {
-    int i;
+    void *nodeValue = malloc(valueSize);
+    coppyBytes(nodeValue, value, valueSize);
 
-    for (i = 0; i < index; ++i)
-    {
-        if (Dll->next == NULL && (i != (index - 1)))
-        {
-            return -1;
-        }
-        Dll = Dll->next;
-    }
-
-    DoubleLinkedList *newNode = initDLList(newValue, valueSize);
+    Node *newNode = (Node *)malloc(sizeof(Node));
     if (newNode == NULL)
     {
         return -1;
     }
 
-    if (Dll != NULL && Dll->prev != NULL)
+    newNode->next = *tail;
+    newNode->prev = NULL;
+    newNode->value = nodeValue;
+
+    if (*tail != NULL)
     {
-        newNode->prev = Dll->prev;
-        Dll->prev->next = newNode;
+        (*tail)->prev = newNode;
     }
 
-    printf("here");
-    newNode->next = Dll;
-    Dll->prev = newNode;
+    *tail = newNode;
     return 0;
+}
+
+int addToHead(Node **head, void *value, size_t valueSize)
+{
+    void *nodeValue = malloc(valueSize);
+    coppyBytes(nodeValue, value, valueSize);
+
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (newNode == NULL)
+    {
+        return -1;
+    }
+
+    newNode->prev = *head;
+    newNode->next = NULL;
+    newNode->value = nodeValue;
+
+    if (*head != NULL)
+    {
+        (*head)->next = newNode;
+    }
+
+    *head = newNode;
+    return 0;
+}
+
+// Returns 0 on successful insertion. -1 if fails
+int insertDLLValueAtIndex(Node **Dll, size_t index, void *newValue, size_t valueSize)
+{
+    int i;
+    Node *ref = *Dll;
+    if (ref == NULL)
+    {
+        return -1;
+    }
+    for (i = 0; i < index; ++i)
+    {
+        if (ref->next == NULL && (i != (index - 1)))
+        {
+            return -1;
+        }
+        else if (ref->next == NULL && (i == (index - 1)))
+        {
+            break;
+        }
+        ref = ref->next;
+    }
+
+    if (index == 0)
+    {
+        addToTail(Dll, newValue, valueSize);
+        return 0;
+    }
+    else if (i == (index - 1))
+    {
+        addToHead(Dll, newValue, valueSize);
+        return 0;
+    }
+
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    void *nodeValue = malloc(valueSize);
+    coppyBytes(nodeValue, newValue, valueSize);
+
+    if (newNode == NULL || nodeValue == NULL)
+    {
+        return -1;
+    }
+
+    if (ref != NULL && ref->prev != NULL)
+    {
+        newNode->prev = ref->prev;
+        ref->prev->next = newNode;
+        newNode->next = ref;
+        ref->prev = newNode;
+    }
+
+    newNode->value = nodeValue;
+    return 0;
+}
+
+int setValueByNode(Node *node, void *newValue, size_t valueSize)
+{
+    node->value = realloc(node->value, valueSize);
+    if (node->value == NULL)
+    {
+        return -1;
+    }
+    coppyBytes(node->value, newValue, valueSize);
+    return 0;
+}
+
+int setListValueByIndex(Node *head, void *newValue, size_t valueSize, size_t index)
+{
+    for (; index > 0; --index)
+    {
+        if (head == NULL)
+        {
+            return -1;
+        }
+        head = head->next;
+    }
+
+    head->value = realloc(head->value, valueSize);
+    if (head->value != NULL)
+    {
+        coppyBytes(head->value, newValue, valueSize);
+        return 0;
+    }
+    return -1;
+}
+
+// To do. Refactor function deleteNodeByIndex
+int deleteNodeByIndex(Node **tail, Node **head, int index)
+{
+    Node *node = *tail;
+
+    for (; index > 0; --index)
+    {
+        if (node == NULL)
+        {
+            return -1;
+        }
+        node = node->next;
+    }
+
+    if (node == NULL)
+    {
+        return -1;
+    }
+
+    if (node->prev != NULL && node->next != NULL)
+    {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+    else if (node->prev == NULL && node->next == NULL)
+    {
+        *tail = NULL;
+        *head = NULL;
+    }
+    else if (node->next == NULL)
+    {
+        node->prev->next = NULL;
+    }
+    else if (node->prev == NULL)
+    {
+        node->next->prev = NULL;
+    }
+
+    free(node->value);
+    free(node);
+    return 1;
+}
+
+void printLinkedListFromTail(Node *tail, void (*printFunction)(void *value))
+{
+    while (tail != NULL)
+    {
+        printFunction(tail->value);
+        tail = tail->next;
+    }
+}
+
+void printInt(void *value)
+{
+    int num = *((int *)value);
+    printf("%d ", num);
 }
 
 int main(int argc, char const *argv[])
 {
-    int value = 123;
-    int value2 = 123456;
-    DoubleLinkedList *newList = initDLList(&value, sizeof(value));
+    int value = 0;
 
-    int *valuePtr = (int *)newList->value;
-    insertDLLValueAtIndex(newList, 1, &value2, sizeof(value2));
-    printf("%d %d", *valuePtr, getDLListLen(newList));
+    Node *head, *tail;
+
+    if (initDLList(&head, &tail, &value, sizeof(value)))
+    {
+        printf("Failed to initialize list\n");
+    }
+    else
+    {
+        printf("Successfuly initialised list\n");
+    }
+    ++value;
+    printf("%d add 1 to tail\n", addToTail(&tail, &value, sizeof(value)));
+    ++value;
+    printf("%d add 2 to tail\n", addToTail(&tail, &value, sizeof(value)));
+    ++value;
+    printf("%d add 3 to head\n", addToHead(&head, &value, sizeof(value)));
+    ++value;
+    printf("%d insert 4 at index 2\n", insertDLLValueAtIndex(&tail, 2, &value, sizeof(value)));
+
+    printf("%d set value at index 0 to value 4\n", setListValueByIndex(tail, &value, sizeof(value), 0));
+
+    printf("%d delete value at index 1\n", deleteNodeByIndex(&tail, &head, 1));
+    printLinkedListFromTail(tail, printInt);
+    int *valuePtr = (int *)tail->value;
+    printf("value at tail: %d; List length: %d\n", *valuePtr, getDLListLen(tail));
 
     return 0;
 }
